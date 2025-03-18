953e7d2a4defb64f9261b026840bccbcd4d695da
anchel.silva <anchel.varela@gmail.com>
2025-03-18 11:53:20 -0300

    Adiciona titulo e unidade de temperatura

--- src/weatherInfos.jsx
+++ src/weatherInfos.jsx
@@ -1,6 +1,20 @@
+import { CloudSunIcon } from "lucide-react";
+
 function WeatherInfos(){
     return (
-        <h1>Weather Infos</h1>
+        <div className="w-[75%] py-10 px-12">
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-row items-center gap-2">
+                    <h1 className="font-bold text-3xl">React Weather</h1>
+                    <CloudSunIcon size={32}/>
+                </div>
+
+                <div className="flex flex-row gap-3">
+                    <button className="font-semibold text-xl text-white bg-black rounded-full py-2 px-3">C°</button>
+                    <button className="font-semibold text-xl bg-white rounded-full py-2 px-3">F°</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
